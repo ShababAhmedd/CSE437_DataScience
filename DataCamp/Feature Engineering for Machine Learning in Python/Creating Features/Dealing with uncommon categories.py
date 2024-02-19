a1253bfa317de9c3584be2a76c8e05791629581e
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-02-19 18:16:02 +0600

    Create Dealing with uncommon categories.py

---
+++ DataCamp/Feature Engineering for Machine Learning in Python/Creating Features/Dealing with uncommon categories.py
@@ -0,0 +1,8 @@
+# Create a series out of the Country column
+countries = so_survey_df['Country']
+
+# Get the counts of each category
+country_counts = countries.value_counts()
+
+# Print the count values for each category
+print(country_counts)
