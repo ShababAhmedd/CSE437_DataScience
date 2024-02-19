0a6acd977af5675d5194b29b156646e99dbdaea1
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-02-19 18:15:26 +0600

    Create One-hot encoding and dummy variables.py

---
+++ DataCamp/Feature Engineering for Machine Learning in Python/Creating Features/One-hot encoding and dummy variables.py
@@ -0,0 +1,5 @@
+# Convert the Country column to a one hot encoded Data Frame
+one_hot_encoded = pd.get_dummies(so_survey_df, columns=['Country'], prefix='OH')
+
+# Print the columns names
+print(one_hot_encoded.columns)
