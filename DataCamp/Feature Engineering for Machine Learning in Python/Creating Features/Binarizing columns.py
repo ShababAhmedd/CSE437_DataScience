241267b0df3b02080aab5db0a3a6cb91f268b11d
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-02-19 21:47:16 +0600

    Create Binarizing columns.py

---
+++ DataCamp/Feature Engineering for Machine Learning in Python/Creating Features/Binarizing columns.py
@@ -0,0 +1,8 @@
+# Create the Paid_Job column filled with zeros
+so_survey_df['Paid_Job'] = 0
+
+# Replace all the Paid_Job values where ConvertedSalary is > 0
+so_survey_df.loc[so_survey_df['ConvertedSalary'] > 0, 'Paid_Job'] = 1
+
+# Print the first five rows of the columns
+print(so_survey_df[['Paid_Job', 'ConvertedSalary']].head())
