558d8b971c6cdca6c68baa3f7b3630b2b0b08c3e
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-03-05 22:44:09 +0600

    Create Define the GB regressor.py

---
+++ DataCamp/Machine Learning with Tree-Based Models in Python/Boosting/Define the GB regressor.py
@@ -0,0 +1,5 @@
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Instantiate gb
+gb=GradientBoostingRegressor(n_estimators=200, max_depth=4, random_state=2)
