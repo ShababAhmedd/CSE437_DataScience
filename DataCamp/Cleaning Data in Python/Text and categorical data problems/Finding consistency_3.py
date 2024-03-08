45cf4d2b4468d43c0847eeca439a49090f7b7024
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-03-08 14:25:23 +0600

    Create Finding consistency_3.py

---
+++ DataCamp/Cleaning Data in Python/Text and categorical data problems/Finding consistency_3.py
@@ -0,0 +1,8 @@
+# Find the cleanliness category in airlines not in categories
+cat_clean = set(airlines['cleanliness']).difference(categories['cleanliness'])
+
+# Find rows with that category
+cat_clean_rows = airlines['cleanliness'].isin(cat_clean)
+
+# Print rows with inconsistent category
+print(airlines[cat_clean_rows])
