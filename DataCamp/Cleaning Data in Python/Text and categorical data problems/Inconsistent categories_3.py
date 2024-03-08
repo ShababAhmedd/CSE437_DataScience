291faa7df8c8bde8141e22bfc027611b2b96ffc3
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-03-08 18:16:38 +0600

    Create Inconsistent categories_3.py

---
+++ DataCamp/Cleaning Data in Python/Text and categorical data problems/Inconsistent categories_3.py
@@ -0,0 +1,7 @@
+# Print unique values of both columns
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Lower dest_region column and then replace "eur" with "europe"
+airlines['dest_region'] = airlines['dest_region'].str.lower()
+airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
