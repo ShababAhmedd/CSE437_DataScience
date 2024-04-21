18bd56c9752d7cc17fdb043d5c020f4ea4d3dc79
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-21 18:07:40 +0600

    Create Binning values2.py

---
+++ DataCamp/Feature Engineering for Machine Learning in Python/Creating Features/Binning values2.py
@@ -0,0 +1,14 @@
+# Import numpy
+import numpy as np
+
+# Specify the boundaries of the bins
+bins = [-np.inf, 10000, 50000, 100000, 150000, np.inf]
+
+# Bin labels
+labels = ['Very low', 'Low', 'Medium', 'High', 'Very high']
+
+# Bin the continuous variable ConvertedSalary using these boundaries
+so_survey_df['boundary_binned'] = pd.cut(so_survey_df['ConvertedSalary'], bins=bins, labels=labels)
+
+# Print the first 5 rows of the boundary_binned column
+print(so_survey_df[['boundary_binned', 'ConvertedSalary']].head())
